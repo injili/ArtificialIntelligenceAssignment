4458529d1ac2b3bb60fa1c786250fe0ffdad3baa
injili <nyarekigospel@gmail.com>
2024-07-22 18:20:32 +0300

    the first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+"""
+App.py - The main file of the entire application
+importations:
+    flask modules
+"""
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """
+        The default function
+    """
+    return "Welcome to the Flask App!"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """"
+        The 404 error page
+    """
+    return "Page not found"
+    # return render_template("404.html"), 404
+
+if __name__ == "__main__":
+    app.run(port=1111, debug=True)
---
+++ test_app.py
@@ -0,0 +1,8 @@
+"""
+    Test_app.py - tests for the app functions
+    To run the pytest - ...
+""" 
+import pytest
+from app import ap
+from app import get_completion
+
